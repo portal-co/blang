d2f3bd9ad2126dc3a8f3962fe180f09a020afd20
Graham Kelly <gkgoat6700@gmail.com>
2025-12-06 14:21:42 -0500

    seperate out lib (part 1)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-use rand::{Rng, SeedableRng};
-use rand_chacha::ChaCha20Rng;
-use sha3::Digest;
+use blang::{create_seeded_rng, generate_random_string};
 
 fn main() -> std::io::Result<()> {
     let mut args = std::env::args();
@@ -10,23 +8,10 @@ fn main() -> std::io::Result<()> {
         Some(a) => a,
         None => (&*kind, ""),
     };
-    let mut hash = sha3::Sha3_256::default();
-    for arg in args {
-        hash.update(&arg);
-        hash.update(&[0xff]);
-    }
-    let mut rng = ChaCha20Rng::from_seed(hash.finalize().into());
+    let mut rng = create_seeded_rng(args);
     for l in std::io::stdin().lines() {
         let l = l?;
-        let r = (0..rng.random_range(2..4))
-            .map(|_| match rng.random_range(0..3) {
-                0 => rng.random_range(0xac00..(0xac00 + 11172)),
-                1 => rng.random_range(0xa000..(0xa000 + 1165)),
-                2 => rng.random_range(0x17000..(0x17000 + 6144)),
-                _ => todo!(),
-            })
-            .filter_map(|a| char::from_u32(a))
-            .collect::<String>();
+        let r = generate_random_string(&mut rng);
         match target {
             "ts:core" => {
                 println!("export const k{}: string = \"{r}\";",l.chars().filter(|a|a.is_alphanumeric()).collect::<String>())
